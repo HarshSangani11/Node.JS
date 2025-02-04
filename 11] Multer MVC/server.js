30d8fd8cdd838fe6c4ffbd6e550a14ea5da9a685
HarshSangani11 <harshsangani2105@gmail.com>
2025-02-04 21:37:56 +0530

    commited

---
+++ 11] Multer MVC/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const port = 2125;
+const path = require("path")
+const app = express();
+
+const db = require("./config/db");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded());
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+
+app.use("/", require("./routes/route"));
+app.use("/add-book", require("./routes/route"));
+app.use("/delete/:id", require("./routes/route"));
+app.use("/edit/:id", require("./routes/route"));
+app.use("/update-book", require("./routes/route"));
+
+app.listen(port, (err) => {
+    err ? console.log("Error : ", err) : console.log("Server is running on port : ", port);
+})
