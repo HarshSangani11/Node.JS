bc8b750dc38a8e634e6599d1a93a165625306da6
HarshSangani11 <harshsangani2105@gmail.com>
2025-03-21 15:37:48 +0530

    commited

---
+++ SEO_admin_panaL/controller/subCategoryCtl.js
@@ -0,0 +1,20 @@
+const catSchema = require("../modal/categorySchema");
+const subCatSchema = require("../modal/subCategorySchema");
+
+module.exports.addSubCategory = async (req,res)=>{
+    await catSchema.find({}).then((data)=>{
+        res.render("addSubCategory",{data})
+    })
+}
+
+module.exports.addSubCategoryData = async(req,res)=>{
+    await  subCatSchema.create(req.body).then(()=>{
+        res.redirect("/subCategory/addSubCategory");
+    })
+}
+
+module.exports.viewSubCategory = async(req,res)=>{
+    await subCatSchema.find({}).populate("categoryId").then(subCategory => {
+        res.render("viewSubCategory", { subCategory });
+    })
+}
---
+++ SEO_admin_panaL/modal/adminSchema.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname:{
+        type:String,
+        required:true
+    },
+    bdate:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    pnumber:{
+        type:Number,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    }
+})
+
+const adminSchema = mongoose.model("Admin",schema);
+module.exports = adminSchema; 
